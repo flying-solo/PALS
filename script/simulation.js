f0f9e8039c8343631429855ba3faa0087c873cd2
Shashank Kumar <shashankkumarthakur@gmail.com>
2022-03-27 13:16:04 +0530

    ✨ Changes to logic js

--- script/simulation.js
+++ script/simulation.js
@@ -65,3 +65,64 @@ function value(id) {
       ];
   }
 }
+
+function displayAnswerpost() {
+  let c = 0;
+  if (document.getElementById("option-11").checked) {
+    c++;
+    document.getElementById("block-11").style.border = "3px solid limegreen";
+    document.getElementById("result-11").style.color = "limegreen";
+    document.getElementById("result-11").innerHTML = "Correct!";
+    alert(`Correct Answer! On to the next.`);
+    window.location = "/html/simulation/simulation4.html";
+  }
+  if (document.getElementById("option-12").checked) {
+    document.getElementById("block-12").style.border = "3px solid red";
+    document.getElementById("result-12").style.color = "red";
+    document.getElementById("result-12").innerHTML = "Incorrect!";
+    document.getElementById("block-11").style.border = "3px solid limegreen";
+    document.getElementById("result-11").style.color = "limegreen";
+    document.getElementById("result-11").innerHTML =
+      "This is the correct answer!";
+  }
+}
+
+function displayAnswer() {
+  let c = 0;
+  if (document.getElementById("option-11").checked) {
+    c++;
+    document.getElementById("block-11").style.border = "3px solid limegreen";
+    document.getElementById("result-11").style.color = "limegreen";
+    document.getElementById("result-11").innerHTML = "Correct!";
+    alert(`Correct Answer! On to the next.`);
+    window.location = "/html/simulation/simulation8.html";
+    return;
+  }
+  if (document.getElementById("option-12").checked) {
+    document.getElementById("block-12").style.border = "3px solid red";
+    document.getElementById("result-12").style.color = "red";
+    document.getElementById("result-12").innerHTML = "Incorrect!";
+    document.getElementById("block-11").style.border = "3px solid limegreen";
+    document.getElementById("result-11").style.color = "limegreen";
+    document.getElementById("result-11").innerHTML =
+      "This is the correct answer!";
+  }
+  if (document.getElementById("option-13").checked) {
+    document.getElementById("block-13").style.border = "3px solid red";
+    document.getElementById("result-13").style.color = "red";
+    document.getElementById("result-13").innerHTML = "Incorrect!";
+    document.getElementById("block-11").style.border = "3px solid limegreen";
+    document.getElementById("result-11").style.color = "limegreen";
+    document.getElementById("result-11").innerHTML =
+      "This is the correct answer!";
+  }
+  if (document.getElementById("option-14").checked) {
+    document.getElementById("block-14").style.border = "3px solid red";
+    document.getElementById("result-14").style.color = "red";
+    document.getElementById("result-14").innerHTML = "Incorrect!";
+    document.getElementById("block-11").style.border = "3px solid limegreen";
+    document.getElementById("result-11").style.color = "limegreen";
+    document.getElementById("result-11").innerHTML =
+      "This is the correct answer!";
+  }
+}
