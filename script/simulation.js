3ef2dc5a47900b5c4dc3013c34afafbcaa6957da
Shashank Kumar <shashankkumarthakur@gmail.com>
2022-03-27 14:14:24 +0530

    🚑 Fix wrong ans

--- script/simulation.js
+++ script/simulation.js
@@ -57,7 +57,10 @@ function value(id) {
     case "ans-sim-1-step-11":
       return ["G1G2G3/(1+G1G2G3H1)", "G1*G2*G3/(1+G1*G2*G3*H1)"];
     case "ans-sim-1-step-11":
-      return ["G1G2G3/(1+G1G2G3H1)", "G1*G2*G3/(1+G1*G2*G3*H1)"];
+      return [
+        "G1G2G3G4(G5+G6)/(1+G1G2G3H1)(1+G4H2(G5+G6))",
+        "G1*G2*G3*G4(G5+G6)/(1+G1*G2*G3*H1)(1+G4*H2*(G5+G6))",
+      ];
     case "ans-sim-1-step-12":
       return [
         "G1G2G3G4(G5+G6)/((1+G1G2G3H1)(1+G4H2(G5+G6)+G3G4H3))",
